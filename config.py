b1e55492f762fe3809ed664220396dd703db3dc3
thebooyakid <alex96740@gmail.com>
2021-06-23 10:23:41 -0400

    new stuff

--- config.py
+++ config.py
@@ -14,5 +14,5 @@ class Config():
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False #Turn off update messages from SQLALCHEMY
